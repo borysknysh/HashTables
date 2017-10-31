f1b88d152422c2c946f530d2a2d6b790a5b80db8
iodapro <borys.knysh@gmail.com>
2017-10-31 18:50:19 +0100

    Create BKLinkedHashTable.h
    Declaration of class of hash table with linked list for resolving collision

---
+++ include/BKLinkedHashTable.h
@@ -0,0 +1,26 @@
+#ifndef BKLINKEDHASHTABLE_H
+#define BKLINKEDHASHTABLE_H
+
+#include <iostream>
+#include "BKLinkedHashEntry.h"
+#include "BKHashInterface.h"
+#include <stdexcept>
+#include <cmath>
+
+namespace BKData
+{
+  template<class K, class V>
+  class BKLinkedHashTable: public BKHashInterface<K,V>{
+  public:
+    BKLinkedHashTable(size_t size = 1e8);
+    ~BKLinkedHashTable();
+    V BKGetValue(K key) override;
+    void BKPutValue(K key, V value) override;
+    K BKHash(K key) override;
+    void BKSetSize(const size_t& size) override;
+  private:
+    BKLinkedHashEntry<K,V>** fTable;
+    size_t fSize;
+  };
+}
+#endif
